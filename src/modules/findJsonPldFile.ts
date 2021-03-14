a7847a0d5dd2b362aa4b1361d76fd2dbfadceeee
Clovis de Villars <clovis.devillars@gmail.com>
2021-03-14 15:16:26 +0100

    feat(modules): add module to find pld JSON file

---
+++ src/modules/findJsonPldFile.ts
@@ -0,0 +1,25 @@
+import fsPromise from 'fs/promises'
+import path from 'path'
+import { is } from 'typescript-is'
+import IPld from '../types/Pld'
+
+export default async function findJsonPldFile(dirPath: string): Promise<{ path: string, content: IPld }> {
+  const jsonFiles = await fsPromise.readdir(dirPath)
+    .then(files => files.filter(file => file.toLowerCase().endsWith('.json')))
+
+  return await Promise.any(jsonFiles
+    .map(filePath => fsPromise.readFile(path.resolve(dirPath, filePath))
+      .then(jsonContent => JSON.parse(jsonContent.toString()))
+      .then(content => {
+        if (!is<IPld>(content)) {
+          throw new Error(`Invalid Pld content : ${ path.resolve(dirPath, filePath) }`)
+        }
+        return {
+          path: filePath,
+          content,
+        }
+      })))
+    .catch((error: AggregateError) => Promise.reject(
+      new AggregateError(error.errors, 'Can not find PLD file'),
+    ))
+}
