a000d9e9d16f4a707b2cae6df031375150a3a589
Clovis de Villars <clovis.devillars@gmail.com>
2021-03-24 13:23:17 +0100

    refactor(cli): fix global options descriptions

--- src/modules/globalCmdOptions.ts
+++ src/modules/globalCmdOptions.ts
@@ -2,9 +2,9 @@ import commander from 'commander'
 import { setScriptMode } from './globals'
 
 const globalCmdOptions = {
-  file: commander.createOption('-f, --file <pld_file_path>', 'Pld file the cli will use'),
-  dir: commander.createOption('-d, --dir <pld_dir_path>', 'Directory where cli will search for pld file'),
-  script: commander.createOption('--script', 'Will print command outputs in JSON and without any logs')
+  file: commander.createOption('-f, --file <pld_file_path>', 'pld file the cli will use'),
+  dir: commander.createOption('-d, --dir <pld_dir_path>', 'directory where cli will search for pld file'),
+  script: commander.createOption('--script', 'will print command outputs in JSON and without any logs')
     .argParser(value => {
       setScriptMode(true)
       return value
