b224cdf15dc43a009874aa166dbdeaf81f1c71c9
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-23 20:47:16 +0200

    feat(cli): add "distribution" command

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@ import updateNotifier from './modules/updateNotifier'
 import versionCommand from './modules/commands/version'
 import durationCommand from './modules/commands/duration'
 import assigneesCommand from './modules/commands/assignees'
+import distributionCommand from './modules/commands/distribution'
 import userStoryCommand from './modules/commands/userStory'
 import globalOptions from './modules/globalCmdOptions'
 import { version } from '../package.json'
@@ -20,6 +21,7 @@ export default async function cli(argv: string[]) {
   program.addCommand(versionCommand)
   program.addCommand(durationCommand)
   program.addCommand(assigneesCommand)
+  program.addCommand(distributionCommand)
   program.addCommand(userStoryCommand)
 
   try {
