6cca41060acc8a5c83563717413f2eeb8875d83b
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-23 02:10:01 +0200

    feat(assignees-cmd): make command argument variadic

--- src/modules/commands/assignees.ts
+++ src/modules/commands/assignees.ts
@@ -1,20 +1,17 @@
 import commander from 'commander'
-import search from './userStory/options/search'
-import status from './userStory/options/status'
+import search, { SearchOption } from './userStory/options/search'
+import status, { StatusOption } from './userStory/options/status'
 import commandHandler from './userStory/utils/commandHandler'
 
+export type Options = StatusOption & SearchOption
+
 export default commander.createCommand('assignees')
-  .arguments('<user>')
+  .arguments('<user...>')
   .description('list user stories assigned to specified user')
   .addOption(status)
   .addOption(search)
-  .action((user, options: Options, command) => commandHandler(command.parent?.opts(), {
+  .action((user: string[], options: Options, command) => commandHandler(command.parent?.opts(), {
     ...options,
-    assignments: [user],
+    assignments: user,
     search: options.search?.join(' '),
   }))
-
-export interface Options {
-  status?: string[]
-  search?: string[]
-}
