132e38f5bc849b33f6f7cf7740aae094ce7284a3
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-23 02:21:27 +0200

    feat(user-story-cmd): add "list" sub command

--- src/modules/commands/userStory.ts
+++ src/modules/commands/userStory.ts
@@ -1,5 +1,7 @@
 import commander from 'commander'
+import listCommand from './userStory/list'
 
 export default commander.createCommand('user-story')
   .aliases(['us', 'story'])
   .description('user stories related commands')
+  .addCommand(listCommand)
