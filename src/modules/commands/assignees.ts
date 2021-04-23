1079e961e4d82e9b6d64b61e58c59930ce245248
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-23 02:02:01 +0200

    refactor(assignees-cmd): use generic "user-story" command handler

--- src/modules/commands/assignees.ts
+++ src/modules/commands/assignees.ts
@@ -1,54 +1,21 @@
-import commander, { Option } from 'commander'
-import getPld from '../getPld'
-import logger from '../logger'
-import { scriptMode } from '../globals'
-import printUserStoryCard from '../printUserStoryCard'
-
-const validStatus = ['to do', 'todo', 'wip', 'done', 'abandoned']
-
-const commandOptions = {
-  status: Object.assign(
-    new Option('-s --status <status>', 'Filter by user story status')
-      .argParser(value => {
-        const status = value.toLowerCase()
-
-        if (!validStatus.includes(status)) {
-          throw new commander.InvalidOptionArgumentError(`Valid status: ${ validStatus.join(', ') }`)
-        }
-
-        return status === 'todo' ? 'to do' : status
-      }),
-    { argChoices: validStatus },
-  ),
-}
+import commander from 'commander'
+import search from './userStory/options/search'
+import status from './userStory/options/status'
+import commandHandler from './userStory/utils/commandHandler'
 
 export default commander.createCommand('assignees')
   .arguments('<user>')
   .description('list user stories assigned to specified user')
-  .addOption(commandOptions.status)
-  .action((user, options: Options, command) => getPld(command.parent?.opts())
-    .then(pld => {
-      const assignees = pld.assignees(user, {
-        status: options.status ? [options.status] : undefined,
-      })
-
-      if (assignees.length === 0) {
-        logger.warn('No user story found')
-        return
-      }
-
-      if (scriptMode()) {
-        logger.log(JSON.stringify({ assignees }, undefined, 2))
-      } else {
-        assignees.forEach(userStory => printUserStoryCard(userStory))
-        logger.info(`${ assignees.length } user stories found`)
-      }
-    })
-    .catch((error: AggregateError) => {
-      logger.error(error.message)
-      process.exit(1)
-    }))
+  .addOption(status)
+  .addOption(search)
+  .action((user, options: Options, command) => commandHandler(command.parent?.opts(), {
+    ...options,
+    assignments: [user],
+    status: options.status ? [options.status] : undefined,
+    search: options.search?.join(' '),
+  }))
 
 export interface Options {
   status?: string
+  search?: string[]
 }
