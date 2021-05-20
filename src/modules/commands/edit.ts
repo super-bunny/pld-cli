10110fd5463256ae648bd1a1c64e71835b43350d
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-20 23:25:55 +0200

    feat(edit-cmd): add "generate-ids" sub command

--- src/modules/commands/edit.ts
+++ src/modules/commands/edit.ts
@@ -1,5 +1,7 @@
 import commander from 'commander'
+import generateIds from './edit/generateIds'
 
 export default commander.createCommand('edit')
   .aliases(['e'])
   .description('group commands to edit pld file')
+  .addCommand(generateIds)
---
+++ src/modules/commands/edit/generateIds.ts
@@ -0,0 +1,40 @@
+import commander from 'commander'
+import chalk from 'chalk'
+import getPld from '../../getPld'
+import logger from '../../logger'
+import { scriptMode } from '../../globals'
+
+export interface Options {
+  overwrite?: boolean
+}
+
+export default commander.createCommand('generate-ids')
+  .aliases(['gen-ids'])
+  .description('generate id for each user story and write it to the pld file')
+  .option('-o --overwrite', 'overwrite existing ids')
+  .action((options: Options, command) => getPld(command.parent?.opts())
+    .then(async pld => {
+      if (options.overwrite) {
+        logger.warn('Overwriting ids!')
+      }
+
+      const generatedIdCount = pld.generateUserStoriesIds({
+        overwrite: options.overwrite,
+      })
+
+      if (scriptMode()) {
+        logger.log(JSON.stringify({ generatedIdCount, pld: pld.content }, undefined, 2))
+        return
+      }
+
+      if (generatedIdCount > 0) {
+        await pld.saveToJsonFile()
+      }
+
+      logger.info(`${ generatedIdCount } id(s) generated`)
+      logger.success(`Modification written to pld file: ${ chalk.yellow(pld.filePath) }`)
+    })
+    .catch((error: AggregateError) => {
+      logger.error(error.message)
+      process.exit(1)
+    }))
