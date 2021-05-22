37398d476ccbd5646f93ef9b93add6f8f34fa952
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-22 13:54:43 +0200

    feat(cli): print all version info if invalid sem version number is found

--- src/modules/commandErrorHandler.ts
+++ src/modules/commandErrorHandler.ts
@@ -1,6 +1,15 @@
+import chalk from 'chalk'
+import printVersionCard from './print/version'
 import logger from './logger'
+import InvalidPldSemVersion from '../lib/classes/errors/InvalidPldSemVersion'
 
 export default function commandErrorHandler(error: Error): void {
+  if (error instanceof InvalidPldSemVersion && error.version) {
+    printVersionCard(error.version, {
+      title: chalk.bgRedBright(`Invalid semantic version number: ${ error.version.version }`),
+    })
+  }
+
   logger.error(error.message)
   process.exit(1)
 }
